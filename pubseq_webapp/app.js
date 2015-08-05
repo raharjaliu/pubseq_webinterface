7c7119499787ea81e537262ab62aa1c2c5919c41
raharjaliu <raharjaliu@gmail.com>
2015-08-05 22:01:25 +0200

    New Cache Implementation. Remote Test.

--- pubseq_webapp/app.js
+++ pubseq_webapp/app.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
-var app = express();
 var http = require('http');
 var exec = require('child_process').exec;
 var fs = require('fs');
+var NodeCache = require("node-cache");
+
+var app = express();
+// init cache
+var myCache = new NodeCache({
+  stdTTL: 3600,
+  checkperiod: 120
+});
 
 // defines default path
 app.use(express.static(path.join(__dirname, 'public')));
@@ -39,7 +46,7 @@ String.prototype.absHashCode = function() {
 // Standard (ES6) startsWith will be natively
 // supported for string
 if (typeof String.prototype.startsWith != 'function') {
-  String.prototype.startsWith = function (str){
+  String.prototype.startsWith = function(str) {
     return this.slice(0, str.length) == str;
   };
 }
@@ -52,6 +59,23 @@ var logStoutSterrErr = function(exec, stout, sterr, err) {
   if (err !== null) {
     console.log(' exec error: ' + err);
   }
+}
+
+// Given list of UniProt IDs, create
+// query in URI component format
+var createQuery = function(listOfUPIDs) {
+
+  var query = '';
+
+  for (var i = 0; i < listOfUPIDs.length; i++) {
+    if (i > 0) {
+      query += ' OR ';
+    }
+    query += ('uniprotid:' + listOfUPIDs[i]);
+  }
+  query = encodeURIComponent(query);
+
+  return query;
 }
 
 // defines server
@@ -80,9 +104,8 @@ app.post('/', function(req, res) {
 
   if (req.body.mode === 'new') {
 
-    console.log("NEW");
-
     // NEW mode
+    console.log("NEW");
 
     var input = req.body.input;
     var content;
@@ -100,54 +123,54 @@ app.post('/', function(req, res) {
     console.log(sequence);
     console.log("content is");
     console.log(content);
-    var fileIn = sequence.absHashCode() + ".in";
-    var fileOut = sequence.absHashCode() + ".out";
-    var fileScript = 'blast_' + sequence.absHashCode() + ".sh";
-    exec('pwd', function(error, stdout, stderr) {
-      logStoutSterrErr('pwd', stdout, stderr, error);
-      var pwd = stdout.trim();
-      var createFile = "echo '" + content + "' > blast/" + fileIn;
-      exec(createFile, function(error, stdout, stderr) {
-        logStoutSterrErr(createFile, stdout, stderr, error);
-        var chmodFile = 'chmod 775 blast/' + fileIn;
-        exec(chmodFile, function(error, stdout, stderr) {
-          logStoutSterrErr(chmodFile, stdout, stderr, error);
-          var createScript = "echo 'blastpgp -a 24 -i " + pwd + "/blast/" + fileIn + " -d /mnt/project/rost_db/data/big/big -e 0.001 -o " + pwd + "/blast/" + fileOut + " -m 8' > blast/" + fileScript;
-          exec(createScript, function(error, stdout, stderr) {
-            logStoutSterrErr(createScript, stdout, stderr, error);
-            var chmodScript = 'chmod 775 blast/' + fileScript;
-            exec(chmodScript, function(error, stdout, stderr, error) {
-              logStoutSterrErr(chmodScript, stdout, stderr, error);
-              var qsubScript = 'sudo -u pubseq qsub blast/' + fileScript;
-              exec(qsubScript, function(error, stdout, stderr, error) {
-                logStoutSterrErr(qsubScript, stdout, stderr, error);
-                postResponse['status'] = 'submitted';
-                postResponse['id'] = sequence.absHashCode();
-                res.json(postResponse);
+
+    value = myCache.get(sequence);
+
+    if (value == undefined) {
+
+      // sequence is not contained in cache
+      console.log("sequence is not contained in cache");
+
+      var fileIn = sequence.absHashCode() + ".in";
+      var fileOut = sequence.absHashCode() + ".out";
+      var fileScript = 'blast_' + sequence.absHashCode() + ".sh";
+      exec('pwd', function(error, stdout, stderr) {
+        logStoutSterrErr('pwd', stdout, stderr, error);
+        var pwd = stdout.trim();
+        var createFile = "echo '" + content + "' > blast/" + fileIn;
+        exec(createFile, function(error, stdout, stderr) {
+          logStoutSterrErr(createFile, stdout, stderr, error);
+          var chmodFile = 'chmod 775 blast/' + fileIn;
+          exec(chmodFile, function(error, stdout, stderr) {
+            logStoutSterrErr(chmodFile, stdout, stderr, error);
+            var createScript = "echo 'blastpgp -a 24 -i " + pwd + "/blast/" + fileIn + " -d /mnt/project/rost_db/data/big/big -e 0.001 -o " + pwd + "/blast/" + fileOut + " -m 8' > blast/" + fileScript;
+            exec(createScript, function(error, stdout, stderr) {
+              logStoutSterrErr(createScript, stdout, stderr, error);
+              var chmodScript = 'chmod 775 blast/' + fileScript;
+              exec(chmodScript, function(error, stdout, stderr, error) {
+                logStoutSterrErr(chmodScript, stdout, stderr, error);
+                var qsubScript = 'sudo -u pubseq qsub blast/' + fileScript;
+                exec(qsubScript, function(error, stdout, stderr, error) {
+                  logStoutSterrErr(qsubScript, stdout, stderr, error);
+                  postResponse['status'] = 'submitted';
+                  postResponse['id'] = sequence.absHashCode();
+                  postResponse['sequence'] = sequence;
+                  res.json(postResponse);
+                });
               });
             });
           });
         });
       });
-    });
-  } else {
+    } else {
 
-    var query;
-    var cursorMark;
+      console.log("sequence is contained in cache");
+      // sequence is contained in cache
 
-    if (req.body.mode === 'update') {
-      // UPDATE mode
-
-      console.log("UPDATE");
-      console.log()
-
-      query = req.body.query;
-      cursorMark = req.body.cursorMark;
-
+      var query = createQuery(value);
       var solrQueryComplete = 'http://localhost:8983/solr/pubseq/select?wt=json&indent=true&q=' +
-                              query +
-                              '&sort=pubdate+desc%2Cpmid+desc%2c&rows%2Cpmid+desc=10&cursorMark=' +
-                              cursorMark;
+        query +
+        '&sort=pubdate+desc%2Cpmid+desc%2c&rows%2Cpmid+desc=10&cursorMark=*';
       postResponse['query'] = query;
 
       http.get(solrQueryComplete, function(resp) {
@@ -168,6 +191,8 @@ app.post('/', function(req, res) {
           //console.log(dataStr);
           var resObj = JSON.parse(dataStr);
           postResponse['respBody'] = resObj;
+          postResponse['status'] = 'done';
+
           res.json(postResponse);
 
         });
@@ -179,97 +204,23 @@ app.post('/', function(req, res) {
         //console.log(e);
         res.json(postResponse);
       });
-
-    } else if (req.body.mode == 'check') {
-      // CHECK mode
-
-      var outFile = req.body.id + '.out';
-      var checkOutFile = 'ls blast/' + outFile;
-      console.log(checkOutFile);
-      exec(checkOutFile, function(error, stdout, stderr) {
-        logStoutSterrErr(checkOutFile, stdout, stderr, error);
-
-        if (stdout.length > 0) {
-          // output file exists
-          var checkNumOflines = 'wc -l blast/' + outFile;
-          exec(checkNumOflines, function(error, stdout, stderr) {
-            logStoutSterrErr(checkNumOflines, stdout, stderr, error);
-            console.log(stdout);
-            var split = (stdout.trim()).split(" ");
-            var numOflines = parseInt(split[0]);
-
-            console.log("number of lines : " + numOflines);
-            if (numOflines > 0) {
-              console.log("parsing files");
-              // output file exists and not empty
-              query = '';
-              var array = fs.readFileSync('blast/' + outFile).toString().split("\n");
-              var counter = 0;
-              for (var i = 0; i < Math.min(5, array.length); i++) {
-                var line = array[i];
-                var split = line.split('\t');
-                var entries = split[1].split('|');
-                if (i > 0) {
-                  query += ' OR ';
-                }
-                query += ('uniprotid:' + entries[2]);
-              }
-              cursorMark = '*';
-              query = encodeURIComponent(query);
-
-              var solrQueryComplete = 'http://localhost:8983/solr/pubseq/select?wt=json&indent=true&q=' + 
-                                      query +
-                                      '&sort=pubdate+desc%2Cpmid+desc%2c&rows%2Cpmid+desc=10&cursorMark=' +
-                                      cursorMark;
-              postResponse['status'] = 'done';
-              postResponse['query'] = query;
-
-              http.get(solrQueryComplete, function(resp) {
-
-                console.log('Got response from Solr index');
-                var dataStr = '';
-
-                resp.on("data", function(chunk) {
-
-                  // 0 indicates success
-                  postResponse['solrStatus'] = 0;
-                  dataStr += chunk;
-
-                });
-
-                resp.on('end', function() {
-
-                  console.log(dataStr);
-                  var resObj = JSON.parse(dataStr);
-                  postResponse['respBody'] = resObj;
-                  res.json(postResponse);
-                });
-
-              }).on('error', function(e) {
-                console.log('Got error from Solr index');
-                // any status > 0 indicates failure
-                postResponse['solrStatus'] = 1;
-                //console.log(e);
-                res.json(postResponse);
-              });
-            } else {
-              // output file exists but empty
-              postResponse['id'] = req.body.id;
-              postResponse['status'] = 'running';
-              res.json(postResponse);
-            }
-          });
-        } else {
-          // output file doesn't exist
-          postResponse['id'] = req.body.id;
-          postResponse['status'] = 'running';
-          res.json(postResponse);
-        }
-      });
     }
 
-    /*
-    if (querySolr) {
+  } else {
+
+    // if mode is not NEW, then mode is either UPDATE or CHECK
+
+    var query;
+    var cursorMark;
+
+    if (req.body.mode === 'update') {
+
+      // UPDATE mode
+      console.log("UPDATE");
+
+      query = req.body.query;
+      cursorMark = req.body.cursorMark;
+
       var solrQueryComplete = 'http://localhost:8983/solr/pubseq/select?wt=json&indent=true&q=' +
         query +
         '&sort=pubdate+desc%2Cpmid+desc%2c&rows%2Cpmid+desc=10&cursorMark=' +
@@ -305,8 +256,124 @@ app.post('/', function(req, res) {
         //console.log(e);
         res.json(postResponse);
       });
+
+    } else if (req.body.mode == 'check') {
+
+      // CHECK mode
+      console.log('CHECK');
+
+      var outFile = req.body.id + '.out';
+      var inFile = re.body.id + '.in';
+      var scriptFile = 'blast_' + re.body.id + ".sh";
+      var checkOutFile = 'ls blast/' + outFile;
+      console.log(checkOutFile);
+      exec(checkOutFile, function(error, stdout, stderr) {
+        logStoutSterrErr(checkOutFile, stdout, stderr, error);
+
+        if (stdout.length > 0) {
+          // output file exists
+          var checkNumOflines = 'wc -l blast/' + outFile;
+          exec(checkNumOflines, function(error, stdout, stderr) {
+            logStoutSterrErr(checkNumOflines, stdout, stderr, error);
+            console.log(stdout);
+            var split = (stdout.trim()).split(" ");
+            var numOflines = parseInt(split[0]);
+
+            console.log("number of lines : " + numOflines);
+            if (numOflines > 0) {
+
+              // output file exists and not empty -> BLAST is finished
+              console.log("parsing files");
+
+              query = '';
+              var array = fs.readFileSync('blast/' + outFile).toString().split("\n");
+              var listOfUPIDs = [];
+              for (var i = 0; i < Math.min(5, array.length); i++) {
+                var line = array[i];
+                var split = line.split('\t');
+                var entries = split[1].split('|');
+                if (i > 0) {
+                  query += ' OR ';
+                }
+                query += ('uniprotid:' + entries[2]);
+                listOfUPIDs.append(entries[2]);
+              }
+
+              // remove BLAST input file
+              var removeBlastIn = 'rm -f blast/' + inFile;
+              exec(removeBlastIn, function(error, stdout, stderr){
+                logStoutSterrErr(removeBlastIn, stdout, stderr, error);
+              });
+
+              // remove BLAST output file
+              var removeBlastIn = 'rm -f blast/' + outFile;
+              exec(removeBlastOut, function(error, stdout, stderr){
+                logStoutSterrErr(removeBlastOut, stdout, stderr, error);
+              });
+
+              // remove BLAST script file
+              var removeBlastScript = 'rm -f blast/' + scriptFile;
+              exec(removeBlastScript, function(error, stdout, stderr){
+                logStoutSterrErr(removeBlastScript, stdout, stderr, error);
+              });
+
+              cursorMark = '*';
+              query = encodeURIComponent(query);
+
+              // add list of uniprot id to cache
+              myCache.set(req.body.sequence, listOfUPIDs);
+
+              var solrQueryComplete = 'http://localhost:8983/solr/pubseq/select?wt=json&indent=true&q=' +
+                query +
+                '&sort=pubdate+desc%2Cpmid+desc%2c&rows%2Cpmid+desc=10&cursorMark=' +
+                cursorMark;
+              postResponse['status'] = 'done';
+              postResponse['query'] = query;
+
+              http.get(solrQueryComplete, function(resp) {
+
+                console.log('Got response from Solr index');
+                var dataStr = '';
+
+                resp.on("data", function(chunk) {
+
+                  // 0 indicates success
+                  postResponse['solrStatus'] = 0;
+                  dataStr += chunk;
+
+                });
+
+                resp.on('end', function() {
+
+                  console.log(dataStr);
+                  var resObj = JSON.parse(dataStr);
+                  postResponse['respBody'] = resObj;
+                  res.json(postResponse);
+                });
+
+              }).on('error', function(e) {
+                console.log('Got error from Solr index');
+                // any status > 0 indicates failure
+                postResponse['solrStatus'] = 1;
+                //console.log(e);
+                res.json(postResponse);
+              });
+            } else {
+              // output file exists but empty -> BLAST is not finished yet
+              postResponse['id'] = req.body.id;
+              postResponse['status'] = 'running';
+              res.json(postResponse);
+            }
+          });
+        } else {
+          // output file doesn't exist -> BLAST is not finished yet
+          postResponse['id'] = req.body.id;
+          postResponse['sequence'] = res.body.sequence;
+          postResponse['status'] = 'running';
+          res.json(postResponse);
+        }
+      });
     }
-    */
   }
 });
 
